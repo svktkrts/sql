f13d6ad51e0327613f8e30af81cef3a5c4d078df
svktkrts <62145882+svktkrts@users.noreply.github.com>
2023-01-04 19:44:31 +0300

    Create odev5.sql

---
+++ odev5.sql
@@ -0,0 +1,6 @@
+--1
+select * from film where title ilike '%n' order by length desc limit 5
+--2
+select * from film where title ilike '%n' order by length asc offset 5 limit 5
+--3
+select * from customer where store_id=1 order by last_name desc limit 4
